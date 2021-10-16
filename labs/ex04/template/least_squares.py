d4c44c7f8ec3da9806dd251e47bb38479fd82e01
wangweiran0129 <wangwr0129@gmail.com>
2021-10-16 15:02:28 +0200

    Finish Lab 4

--- labs/ex04/template/least_squares.py
+++ labs/ex04/template/least_squares.py
@@ -5,7 +5,7 @@ Least Square
 """
 
 import numpy as np
-
+from costs import compute_mse
 
 def least_squares(y, tx):
     """calculate the least squares."""
@@ -19,6 +19,6 @@ def least_squares(y, tx):
     optimal_w = np.linalg.solve(np.dot(tx.T, tx), np.dot(tx.T, y))
     
     # compute_loss(y, tx, w, flag = 0)
-    mse = compute_loss(y, tx, optimal_w)
+    mse = compute_mse(y, tx, optimal_w)
     
     return mse, optimal_w
--- labs/ex04/template/split_data.py
+++ labs/ex04/template/split_data.py
@@ -16,4 +16,19 @@ def split_data(x, y, ratio, seed=1):
     # INSERT YOUR CODE HERE
     # split the data based on the given ratio: TODO
     # ***************************************************
-    raise NotImplementedError
+    # raise NotImplementedError
+    
+    # Change the order of samples
+    index = np.random.permutation(x.shape[0])
+    split_location = int(np.floor(ratio * x.shape[0]))
+    
+    index_train = index[:split_location]
+    index_test = index[split_location:]
+    
+    x_train = x[index_train]
+    x_test = x[index_test]
+    
+    y_train = y[index_train]
+    y_test = y[index_test]
+    
+    return x_train, y_train, x_test, y_test
